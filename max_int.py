01deb2a184d398091bbd634286c02eb6399842b1
Steinar Páll Magnússon <steinarpallmagnusson@gestur-012.ru.is>
2019-09-05 18:52:46 +0000

    k

--- max_int.py
+++ max_int.py
@@ -1,13 +1,18 @@
-#
+#Design an algorithm that finds the maximum positive integer
+# input by a user.  
+# The user repeatedly inputs numbers until a negative value
+# is entered.
+
+
 
 u_number=0
 max_number=0
 
 while 1:
-    u_number=int(input("Enter a number "))
-    print(u_number)
+    u_number=int(input("Input a number: "))
+    #print(u_number)
     if u_number>max_number:
-        max_number=u_number
-    print(max_number)
+        max_number=u_number 
     if u_number<0:
         break
+print("The maximum is",max_number)
